0af1e8f752b3cc084407a7cbf8ecdeb5aeb2df9f
Craig Dennis <craig@deliverysoftware.co.uk>
2024-12-23 13:40:29 +0000

    Update shopping cart basket component: enhance dropdown styling and add product details

--- src/resources/views/livewire/shopping-cart-basket.blade.php
+++ src/resources/views/livewire/shopping-cart-basket.blade.php
@@ -1,6 +1,8 @@
-<div x-data="{ open: false, timer: null }"
+<div
+    x-data="{ open: true, timer: null }"
     @mouseenter="clearTimeout(timer); open = true"
     @mouseleave="timer = setTimeout(() => open = false, 300)"
+    class="relative"
 >
     <div class="group relative flex items-center h-full content-center px-4 text-slate-400 transition-colors">
         <i class="fas fa-shopping-cart text-xl group-hover:text-primary-500"></i>
@@ -9,17 +11,33 @@
         </div>
     </div>
 
+    {{-- Currentcart dropdown --}}
     <div
         x-show="open"
         x-transition
-        class="z-30 absolute right-0 top-full mt-1 w-72 px-2 py-2 bg-white border border-slate-300 text-slate-400 shadow-lg rounded-md"
+        style="top: calc(100% + 10px);"
+        class="z-30 absolute right-0 w-72 px-1 py-1 bg-white border border-slate-300 text-slate-600 shadow-lg rounded-md"
     >
-        {{-- Dummy items for now --}}
-        <div class="flex justify-between items-center gap-2">
-            <img src="https://via.placeholder.com/64" alt="Product" class="w-16 h-16" />
-            <div>
-                <h3 class="text-lg font-semibold">Product Name</h3>
-                <p class="text-sm text-slate-400">Quantity: 1</p>
+        <div class="flex flex-col gap-1">
+            {{-- Product example --}}
+            <div class="flex justify-between items-center gap-2 px-1 py-1 bg-slate-100 border border-slate-200 rounded-md">
+                <img src="https://via.placeholder.com/64" alt="Product" class="w-16 h-16 border border-slate-300 rounded-md" />
+                <div class="text-sm">
+                    <p>Product Name</p>
+                    <p class="text-slate-400">Quantity: 1</p>
+                    <p class="text-slate-400">Price: £2.50</p>
+                </div>
+            </div>
+
+            {{-- <hr class="w-full border-slate-200" /> --}}
+
+            <div class="flex justify-between items-center gap-2 px-1 py-1 bg-slate-100 border border-slate-200 rounded-md">
+                <img src="https://via.placeholder.com/64" alt="Product" class="w-16 h-16 border border-slate-300 rounded-md" />
+                <div class="text-sm">
+                    <p>Product Name</p>
+                    <p class="text-slate-400">Quantity: 1</p>
+                    <p class="text-slate-400">Price: £2.50</p>
+                </div>
             </div>
         </div>
     </div>
