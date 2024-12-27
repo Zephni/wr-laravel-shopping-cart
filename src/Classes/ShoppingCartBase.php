c64c27fd0d39553fa09fd810a1991912557ddc88
Craig Dennis <craig@deliverysoftware.co.uk>
2024-12-27 10:32:23 +0000

    Implement shopping cart item management methods and add CartItem trait for model integration

--- src/Classes/ShoppingCartBase.php
+++ src/Classes/ShoppingCartBase.php
@@ -1,14 +1,23 @@
 <?php
 namespace WebRegulate\LaravelShoppingCart\Classes;
 
+use Illuminate\Database\Eloquent\Model;
+use WebRegulate\LaravelShoppingCart\Classes\Traits\CartItem;
+
 abstract class ShoppingCartBase
 {
     /**
-     * Data 
+     * Shopping cart data. Note that each item in the cart MUST use this format (use the buildCartItemData method from the CartItem trait):
+     * [
+     *     'model' => 'App\Models\Product', // The product model associated with this item. Note that the model must implement the CartItem trait and set it's getCartName, getCartOptions, and getCartPrice methods
+     *     'modelId' => 1, // The ID of the product model
+     *     'quantity' => 1, // The quantity of this item
+     *     'options' => [] // The options of this item
+     * ]...
      * 
      * @var array
      */
-    protected array $data;
+    protected array $shoppingCartData = [];
 
     /**
      * Saves current data to storage
@@ -25,4 +34,81 @@ abstract class ShoppingCartBase
      * @return void
      */
     abstract public function load(string $uniqueId): void;
+
+    /**
+     * Add cart item, adds new, or increments quantity and merges options if item already exists
+     * 
+     * @param Model $model
+     * @param float $quantity
+     * @param array $options
+     */
+    public function addCartItem(Model $model, float $quantity = 1, array $options = []): void
+    {
+        // Check valid cart item model
+        $this->throwExceptionIfInvalidCartItemModel($model);
+
+        // If item already exists in cart, merge options and update quantity
+        foreach ($this->shoppingCartData as $key => $item) {
+            if ($item['model'] == get_class($model) && $item['modelId'] == $model->id) {
+                $this->shoppingCartData[$key]['quantity'] += $quantity;
+                $this->shoppingCartData[$key]['options'] = array_merge($this->shoppingCartData[$key]['options'], $options);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Set cart item, forcibly sets quantity and options of existing cart item
+     * 
+     * @param Model $model
+     * @param float $quantity
+     * @param array $options
+     */
+    public function setCartItem(Model $model, float $quantity = 1, array $options = []): void
+    {
+        // Check valid cart item model
+        $this->throwExceptionIfInvalidCartItemModel($model);
+
+        // If item already exists in cart, update quantity
+        foreach ($this->shoppingCartData as $key => $item) {
+            if ($item['model'] == get_class($model) && $item['modelId'] == $model->id) {
+                $this->shoppingCartData[$key]['quantity'] = $quantity;
+                $this->shoppingCartData[$key]['options'] = $options;
+                return;
+            }
+        }
+    }
+
+    /**
+     * Remove cart item
+     * 
+     * @param Model $model
+     * @param array $options
+     */
+    public function removeCartItem(Model $model, array $options = []): void
+    {
+        // Check valid cart item model
+        $this->throwExceptionIfInvalidCartItemModel($model);
+
+        // If item already exists in cart, remove it
+        foreach ($this->shoppingCartData as $key => $item) {
+            if ($item['model'] == get_class($model) && $item['modelId'] == $model->id) {
+                unset($this->shoppingCartData[$key]);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Throw exception if invalid cart item model
+     * 
+     * @param Model $model
+     * @return void
+     */
+    private function throwExceptionIfInvalidCartItemModel(Model $model): void
+    {
+        if (!in_array(CartItem::class, class_uses($model))) {
+            throw new \Exception('Model '.$model::class.' must use the WebRegulate\LaravelShoppingCart\Classes\Traits\CartItem trait');
+        }
+    }
 }
---
+++ src/Classes/Traits/CartItem.php
@@ -0,0 +1,49 @@
+<?php
+namespace WebRegulate\LaravelShoppingCart\Classes\Traits;
+
+trait CartItem
+{
+    /**
+     * Get the name to display in the cart for this item
+     * 
+     * @param float $quantity
+     * @param array $options
+     * @return string
+     */
+    public abstract function getCartName(float $quantity, array $options): string;
+
+    /**
+     * Get the options to display in the cart for this item
+     * 
+     * @param float $quantity
+     * @param array $options
+     * @return array
+     */
+    public abstract function getCartOptions(float $quantity, array $options): string;
+
+    /**
+     * Get the price to display in the cart for this item
+     * 
+     * @param float $quantity
+     * @param ?array $options
+     * @return float
+     */
+    public abstract function getCartPrice(float $quantity, ?array $options = null): float;
+
+    /**
+     * Build cart item data
+     * 
+     * @param float $quantity
+     * @param array $options
+     * @return array
+     */
+    public function buildCartItemData(float $quantity, array $options): array
+    {
+        return [
+            'model' => get_class($this),
+            'modelId' => $this->id,
+            'quantity' => $quantity,
+            'options' => $options
+        ];
+    }
+}
